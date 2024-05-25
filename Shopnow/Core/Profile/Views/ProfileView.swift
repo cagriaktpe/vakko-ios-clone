3466ae57f10101d3ba1a2d498e7ee4490fec6a71
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-05-25 16:33:36 +0300

    Update

--- Shopnow/Core/Profile/Views/ProfileView.swift
+++ Shopnow/Core/Profile/Views/ProfileView.swift
@@ -8,37 +8,63 @@
 import SwiftUI
 
 struct ProfileView: View {
-    
     @StateObject private var viewModel = ProfileViewModel()
-    
+
     @Binding var showSignInView: Bool
     @Binding var tabSelection: Int
-    
+
     var body: some View {
         List {
-            if let user = viewModel.user {
-                Section(header: Text("User Information")) {
-                    HStack {
-                        Text("UserId:")
-                        Spacer()
-                        Text(user.userId)
-                    }
+            header
+                .listRowInsets(EdgeInsets())
 
-                    HStack {
-                        Text("Is Anonymous:")
-                        Spacer()
-                        Text(user.isAnonymous?.description.capitalized ?? "N/A")
-                    }
+            NavigationLink(destination: Text("SİPARİŞLERİM")) {
+                Text("SİPARİŞLERİM")
+            }
 
-                    HStack {
-                        Text("Email:")
-                        Spacer()
-                        Text(user.email ?? "N/A")
-                    }
-                }
+            NavigationLink(destination: Text("İPTAL ve İADE İŞLEMLERİ")) {
+                Text("İPTAL ve İADE İŞLEMLERİ")
+            }
+
+            NavigationLink(destination: Text("FAVORİLERİM")) {
+                Text("FAVORİLERİM")
+            }
+
+            NavigationLink(destination: Text("HESABIM")) {
+                Text("HESABIM")
+            }
+
+            NavigationLink(destination: Text("SHOPNOW CARD")) {
+                Text("SHOPNOW CARD")
+            }
+
+            NavigationLink(destination: Text("ADRESLERİM")) {
+                Text("ADRESLERİM")
+            }
+
+            NavigationLink(destination: Text("EPOSTA DEĞİŞTİRME")) {
+                Text("EPOSTA DEĞİŞTİRME")
+            }
+
+            NavigationLink(destination: Text("ŞİFRE DEĞİŞTİRME")) {
+                Text("ŞİFRE DEĞİŞTİRME")
+            }
+
+            NavigationLink(destination: Text("KUPONLARIM")) {
+                Text("KUPONLARIM")
+            }
+
+            NavigationLink(destination: Text("İLETİŞİM İZİNLERİ")) {
+                Text("İLETİŞİM İZİNLERİ")
+            }
+
+            NavigationLink(destination: Text("ÇIKIŞ YAP")) {
+                Text("ÇIKIŞ YAP")
             }
         }
-        .navigationTitle("Profile")
+        .listStyle(.plain)
+        .navigationTitle("PROFİLİM")
+        .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 NavigationLink(destination: SettingsView(showSignedInView: $showSignInView)) {
@@ -59,6 +85,30 @@ struct ProfileView: View {
         .task {
             try? await viewModel.loadCurrentUser()
         }
+    }
+}
+
+extension ProfileView {
+    var header: some View {
+        VStack(spacing: 5) {
+            if let name = viewModel.user?.name,
+               let surname = viewModel.user?.surname {
+                Text("\(name) \(surname)")
+                    .foregroundStyle(.white)
+                    .font(.title3)
+                    .fontWeight(.bold)
+            }
+
+            if let email = viewModel.user?.email {
+                Text(email)
+                    .font(.subheadline)
+                    .foregroundStyle(.white)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .frame(maxHeight: .infinity, alignment: .center)
+        .padding(.vertical, 5)
+        .background(Color.accentColor)
     }
 }
 
